9b7646fb2799b7e69b8f6249d97822db1fc7a850
Diablo <yagnik@gmail.com>
2025-12-27 13:31:00 +0530

    System Redisgn

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'; // Ensure this points to your new App.tsx
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
